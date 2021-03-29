1c63db6636ce2a3eea2436c5180e6fa2c2c2053e
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-29 10:10:53 -0600

    Renderizado de vistas - Layout y template del home

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const Hapi = require('@hapi/hapi')
 const inert = require('@hapi/inert')
+const vision = require('@hapi/vision')
 const path = require('path')
+const handlebars = require('handlebars')
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
@@ -19,15 +21,30 @@ async function init () {
   try {
     // REGISTRAR LOS PLUGINS QUE HAPI VA A NECESITAR PARA SERVIR ARCHIVOS ESTATICOS
     await server.register(inert)
+    await server.register(vision)// Registrar plugin para gestionar el motor de plantillas
+
+    // Configurar nuestro motor de plantillas. Usará handlebars y cuando invoquemos una vista automáticamente buscará una con extensión hbs (no hace falta especificarlo). Debe buscar a partir del directorio actual, las vistas se encuentran en views y se activa compatibilidad con layouts, los cuales se encuentran en layouts
+    server.views({
+      engines: { // --- hapi puede usar diferentes engines
+        hbs: handlebars // --- asociamos el plugin al tipo de archivos  
+      },
+      relativeTo: __dirname, // --- para que las vistas las busque fuera de /public
+      path: 'views', // --- directorio donde colocaremos las vistas dentro de nuestro proyecto
+      layout: true, // --- indica que usaremos layouts 
+      layoutPath: 'views' // --- ubicación de los layouts
+    })
 
     // DEFINICIÓN DE RUTAS SE INDICA EL MÉTODO HTTP, URL Y CONTROLADOR/handler DE RUTA
     // SE DECLARAN DESPUÉS DEL PLUGIN YA QUE LAS RUTAS HACEN USO DEL MISOM PARA DEVOLVER ARCHIVOS ESTATICOS
     server.route({
       method: 'GET',
-      path: '/home',
+      path: '/',
       handler: (request, h) => {
-        // POR EL INERT SE TIENEN ACCESO AL METODO h.file()
-        return h.file('index.html')
+        // El plugin de vision inyecta el metodo view al objeto h para renderizar una vista que seria index.hbs
+        return h.view('index', {
+          // El plugin de vision inyecta los parametros que requiere el layout.hbs
+          title: 'Home'
+        })
       }
     })
 
