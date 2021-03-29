8afcb52a80eb70db30644723954c3ef53961cc12
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-29 09:37:34 -0600

    Uso de plugins - Creando Servidor de Contenido estático

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const inert = require('@hapi/inert')
+const path = require('path')
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
-  host: 'localhost'
+  host: 'localhost',
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, 'public')
+    }
+  }
 })
 
 async function init () {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      // h.response(): Crea un objeto de respuesta.
-      return h.response('Hola Mundo ...').code(200)
-    }
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/redirect',
-    handler: (request, h) => {
-      // h.redirect(): Redirecciona una petición.
-      return h.redirect('http://platzi.com')
-    }
-  })
-
   try {
+    await server.register(inert)
+
+    server.route({
+      method: 'GET',
+      path: '/home',
+      handler: (request, h) => {
+        // h.response(): Crea un objeto de respuesta.
+        return h.file('index.html')
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          index: ['index.html']
+        }
+      }
+    })
+
     await server.start()
   } catch (error) {
     console.error(error)
